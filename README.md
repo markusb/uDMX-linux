9d656a2859dc19749a2f44fe2fc0c887d3f3317e
Markus Baertschi <markus@markus.org>
2014-12-31 19:20:03 +0100

    Updated uDMX, works pretty well.

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 uDMX-linux
 ==========
 
-This is a small command-line utility to drive and test the www.anyma.ch/uDMX interface.
+This is a small command-line utility to drive the www.anyma.ch/uDMX interface.
+It allows to set DMX values manually from the commandline. Fixtures and
+values can be predefined with aliases in a rc file.
 
-The initial source code comes from http://udmx.googlecode.com/.i
+The initial source code comes from http://udmx.googlecode.com/.
 It compiles with minimal tweaks, but the resulting code core-dumps.
 
-This is cleaned-up and partially rewritten uDMX program.
+This is a cleaned-up and improved version, with alias support built in.
 
 You need a uDMX interface to use it, of course.
 
-Markus
+Markus Baertschi, January 2015
 
 markus@markus.org
 
--- uDMX.c
+++ uDMX.c
@@ -1,6 +1,9 @@
-/* Name: powerSwitch.c
- * Project: PowerSwitch based on AVR USB driver
- * Author: Christian Starkjohann
+/*
+ * Name: uDMX.c
+ * Project: Commandline utility for uDMX interface
+ * Author: Markus Baertschi
+ * Based on work from: Christian Starkjohann, 2005-01-16
+ *
  * Creation Date: 2005-01-16
  * Tabsize: 4
  * Copyright: (c) 2005 by OBJECTIVE DEVELOPMENT Software GmbH
@@ -16,150 +19,287 @@ Linux, FreeBSD, Mac OS X and other Unix operating systems. Libusb can be
 obtained from http://libusb.sourceforge.net/.
 */
 
+#define UDMXVERSION "1.0"
+
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <usb.h>    /* this is libusb, see http://libusb.sourceforge.net/ */
+#include <errno.h>
+#include <usb.h> /* this is libusb, see http://libusb.sourceforge.net/ */
+#include "uDMX_cmds.h"
 
-#define USBDEV_SHARED_VENDOR    0x16C0  /* VOTI */
-#define USBDEV_SHARED_PRODUCT   0x05DC  /* Obdev's free shared PID */
+#define USBDEV_SHARED_VENDOR 0x16C0  /* Obdev's free shared VID */
+#define USBDEV_SHARED_PRODUCT 0x05DC /* Obdev's free shared PID */
 /* Use obdev's generic shared VID/PID pair and follow the rules outlined
  * in firmware/usbdrv/USBID-License.txt.
  */
 
-#include "uDMX_cmds.h"
+int debug = 0;
+int verbose = 0;
 
-static void usage(char *name)
-{
+static void usage(char *name) {
     fprintf(stderr, "usage:\n");
-    fprintf(stderr, "  %s <channel> <value> [<value> ...]\n", name);
+    fprintf(stderr, "  %s [-v] [-d] <channel> <value> [<value> ...]\n", name);
+    fprintf(stderr, "     -v          	verbose, display interesting runtime info\n");
+    fprintf(stderr, "     -d          	debug, display debugging info\n");
+    fprintf(stderr, "  %s -bootloader   start bootloader for firmware update\n", name);
 }
 
+static int usbGetStringAscii(usb_dev_handle *dev, int index, int langid,
+                             char *buf, int buflen) {
+    char buffer[256];
+    int rval, i;
 
-static int  usbGetStringAscii(usb_dev_handle *dev, int index, int langid, char *buf, int buflen)
-{
-char    buffer[256];
-int     rval, i;
-
-    if((rval = usb_control_msg(dev, USB_ENDPOINT_IN, USB_REQ_GET_DESCRIPTOR, (USB_DT_STRING << 8) + index, langid, buffer, sizeof(buffer), 1000)) < 0)
+    if ((rval = usb_control_msg(dev, USB_ENDPOINT_IN, USB_REQ_GET_DESCRIPTOR,
+                                (USB_DT_STRING << 8) + index, langid, buffer,
+                                sizeof(buffer), 1000)) < 0)
         return rval;
-    if(buffer[1] != USB_DT_STRING)
+    if (buffer[1] != USB_DT_STRING)
         return 0;
-    if((unsigned char)buffer[0] < rval)
+    if ((unsigned char)buffer[0] < rval)
         rval = (unsigned char)buffer[0];
     rval /= 2;
     /* lossy conversion to ISO Latin1 */
-    for(i=1;i<rval;i++){
-        if(i > buflen)  /* destination buffer overflow */
+    for (i = 1; i < rval; i++) {
+        if (i > buflen) /* destination buffer overflow */
             break;
-        buf[i-1] = buffer[2 * i];
-        if(buffer[2 * i + 1] != 0)  /* outside of ISO Latin1 range */
-            buf[i-1] = '?';
+        buf[i - 1] = buffer[2 * i];
+        if (buffer[2 * i + 1] != 0) /* outside of ISO Latin1 range */
+            buf[i - 1] = '?';
     }
-    buf[i-1] = 0;
-    return i-1;
+    buf[i - 1] = 0;
+    return i - 1;
 }
 
-
-/* PowerSwitch uses the free shared default VID/PID. If you want to see an
- * example device lookup where an individually reserved PID is used, see our
- * RemoteSensor reference implementation.
+/*
+ * uDMX uses the free shared default VID/PID.
+ * To avoid talking to some other device we check the vendor and
+ * device strings returned.
  */
-static usb_dev_handle   *findDevice(void)
-{
-struct usb_bus      *bus;
-struct usb_device   *dev;
-usb_dev_handle      *handle = 0;
+static usb_dev_handle *findDevice(void) {
+    struct usb_bus *bus;
+    struct usb_device *dev;
+    char string[256];
+    int len;
+    usb_dev_handle *handle = 0;
 
     usb_find_busses();
     usb_find_devices();
-    for(bus=usb_busses; bus; bus=bus->next){
-        for(dev=bus->devices; dev; dev=dev->next){
-            if(dev->descriptor.idVendor == USBDEV_SHARED_VENDOR && dev->descriptor.idProduct == USBDEV_SHARED_PRODUCT){
-                char    string[256];
-                int     len;
-                handle = usb_open(dev); /* we need to open the device in order to query strings */
-                if(!handle){
-                    fprintf(stderr, "Warning: cannot open USB device: %s\n", usb_strerror());
+    for (bus = usb_busses; bus; bus = bus->next) {
+        for (dev = bus->devices; dev; dev = dev->next) {
+            if (dev->descriptor.idVendor == USBDEV_SHARED_VENDOR &&
+                dev->descriptor.idProduct == USBDEV_SHARED_PRODUCT) {
+                if (debug) { printf("Found device with %x:%x\n",
+                               USBDEV_SHARED_VENDOR, USBDEV_SHARED_PRODUCT); }
+
+                /* open the device to query strings */
+                handle = usb_open(dev);
+                if (!handle) {
+                    fprintf(stderr, "Warning: cannot open USB device: %s\n",
+                            usb_strerror());
                     continue;
                 }
-                /* now find out whether the device actually is obdev's Remote Sensor: */
-                len = usbGetStringAscii(handle, dev->descriptor.iManufacturer, 0x0409, string, sizeof(string));
-                if(len < 0){
-                    fprintf(stderr, "warning: cannot query manufacturer for device: %s\n", usb_strerror());
+
+                /* now find out whether the device actually is a uDMX */
+                len = usbGetStringAscii(handle, dev->descriptor.iManufacturer,
+                                        0x0409, string, sizeof(string));
+                if (len < 0) {
+                    fprintf(stderr, "warning: cannot query manufacturer for device: %s\n",
+                            usb_strerror());
                     goto skipDevice;
                 }
-                /* fprintf(stderr, "seen device from vendor ->%s<-\n", string); */
-                if(strcmp(string, "www.anyma.ch") != 0)
+                if (debug) { printf("Device vendor is %s\n",string); }
+                if (strcmp(string, "www.anyma.ch") != 0)
                     goto skipDevice;
+
                 len = usbGetStringAscii(handle, dev->descriptor.iProduct, 0x0409, string, sizeof(string));
-                if(len < 0){
+                if (len < 0) {
                     fprintf(stderr, "warning: cannot query product for device: %s\n", usb_strerror());
                     goto skipDevice;
                 }
-                /* fprintf(stderr, "seen product ->%s<-\n", string); */
-                if(strcmp(string, "uDMX") == 0)
+                if (debug) { printf("Device product is %s\n",string); }
+                if (strcmp(string, "uDMX") == 0)
                     break;
-skipDevice:
+
+            skipDevice:
                 usb_close(handle);
                 handle = NULL;
             }
         }
-        if(handle)
+        if (handle)
             break;
     }
-    if(!handle)
-        fprintf(stderr, "Could not find USB device www.anyma.ch/uDMX\n");
     return handle;
 }
 
-int main(int argc, char **argv)
-{
-usb_dev_handle      *handle = NULL;
-unsigned char       buffer[8];
-int                 nBytes;
-        usb_set_debug(1);
-   
+char * read_uDMXrc (char * keyword1, char * keyword2) {
+    FILE *uDMXrc;
+    char uDMXrcname[1024];
+    char *HOME, *p, *linebuf;
+    char keyword[1024];
+    size_t len = 0;
+    ssize_t read;
+
+    HOME=getenv("HOME");
+    strcpy(uDMXrcname,HOME);
+    strcat(uDMXrcname,"/.uDMXrc");
+    uDMXrc = fopen(uDMXrcname,"r");
+    if (! uDMXrc) { 
+        if (debug) {
+            perror("Opening $HOME/.uDMXrc failed");
+        }
+        return 0;
+    }
+
+    sprintf(keyword,"%s %s ",keyword1,keyword2);
+    while ((read = getline(&linebuf, &len, uDMXrc)) != -1) {
+        linebuf[strlen(linebuf)-1]='\0';
+//        printf("Line: '%s', looking for '%s'\n",linebuf,keyword);
+        if (linebuf[0]=='#') { continue; }
+        if ((p=strcasestr(linebuf,keyword))!=NULL) {
+//            printf(">>> Found '%s' in line '%s'\n",keyword,linebuf);
+//            printf("<<< %ld %ld\n",(linebuf-p)+strlen(keyword),&p);
+            return p+strlen(keyword);
+        }
+    }
+    fclose(uDMXrc);
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    usb_dev_handle *handle = NULL;
+    char buffer[8];
+    char *linebuf, *token;
+    int i,nBytes;
+    int arg,val=0,channel=0;
+    unsigned char values[512];
+    usb_set_debug(0);
+
+    /*
+     * Commandline - simple flags
+     *
+     * The argument '-bootloader' als only argument starts the bootloader and exits
+     * The firmware download is then performed separately with uboot utility
+     * The flags '-d' and '-v' provide more verbose output
+     */
+    if (argc == 2 && !strcmp(argv[1], "-bootloader")) {
+        printf("Starting bootloader...\nPlease use the ./uboot utility to "
+               "update firmware.\n");
+        nBytes = usb_control_msg(
+            handle, USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_IN,
+            cmd_StartBootloader, 0, 0, buffer, sizeof(buffer), 5000);
+        exit(0);
+    }
+
+    for (arg=1; arg<argc; arg++) {
+        if (argv[arg][0]=='-') {
+            if (argv[arg][1]=='v') { verbose=1; continue; }
+            if (argv[arg][1]=='d') { debug=1; continue; }
+            usage(argv[0]);
+            exit(1);
+        }
+        break;
+    }
+    if (verbose) fprintf(stderr, "uDMX utility, version %s\n",UDMXVERSION);
+    /*
+     * USB Initialisation
+     *
+     * If we can not find the uDMX device we exit
+     */
     usb_init();
-    if((handle = findDevice()) == NULL){
-        fprintf(stderr, "Could not find USB device \"uDMX\" with vid=0x%x pid=0x%x\n", USBDEV_SHARED_VENDOR, USBDEV_SHARED_PRODUCT);
+    handle = findDevice();
+    if (handle == NULL) {
+        fprintf(stderr,
+                "Could not find USB device www.anyma.ch/uDMX (vid=0x%x pid=0x%x)\n",
+                USBDEV_SHARED_VENDOR, USBDEV_SHARED_PRODUCT);
         exit(1);
     }
-        if(argc < 3){
-                if (strcmp(argv[1], "-bootloader") == 0) {
-                        nBytes = usb_control_msg(handle, USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_IN,
-                                                                        cmd_StartBootloader, 0, 0, buffer, sizeof(buffer), 5000);
-                        printf("Starting bootloader...\nPlease use the ./uboot utility to update firmware.");                                           
 
-                } else {
-                        usage(argv[0]);
-                exit(1);
+    /* at this point we need at lead two aguments - channel and value(s) */
+    if ((argc-arg) < 2) {
+        usage(argv[0]);
+        exit(1);
+    }
+
+//    printf("verbose=%d  debug=%d  arg=%i args=%i  next=%s\n",verbose,debug,arg,argc-arg,argv[arg]);
+
+    /* next argument must be the channel, look it up in uDMXrc */
+    linebuf=read_uDMXrc("channel",argv[arg]);
+    if (linebuf) {
+        channel = atoi(linebuf);
+//        printf("Found channel '%s' as '%s'\n",argv[arg],linebuf);
+    } else {
+        channel = atoi(argv[arg]);
+    }
+    if ((channel<1) || (channel>512)) {
+        fprintf(stderr,"Error: Channel '%i' out of range, must be between 1 and 512 !\n",channel);
+        usage(argv[0]);
+        exit(1);
+    }
+    arg++;
+
+    /* remaining arguments must be value, look them up one by one */
+    for (;arg<argc;arg++) {
+//        printf("Looking up arg %d - %s\n",arg,argv[arg]);
+        linebuf=read_uDMXrc("values",argv[arg]);
+        if (linebuf) {
+//            printf("Found values '%s' as '%s'\n",argv[arg],linebuf);
+            token=strtok(linebuf," ");
+            while (token != NULL) {
+                values[val]=atoi(token);
+//                printf("Token %s int %d\n",token,values[val]);
+                if ((values[val]<0)||(values[val]>256)) {
+                    fprintf(stderr,"Error: Value '%d' in '%s' out of range, must be 0-255\n",values[val],argv[arg]);
+                    exit(1);
+                }
+                val++;
+                token=strtok(NULL," ");
             }
-    }
-        if(argc == 3) {
-        int channel = 0, value = 0;
-        channel = atoi(argv[1]);
-                value = atoi(argv[2]);
-                nBytes = usb_control_msg(handle, USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_OUT,
-                                                                        cmd_SetSingleChannel, value, channel, buffer, sizeof(buffer), 5000);
-        fprintf(stderr, "bytes returned: %i\n", nBytes);
-                if(nBytes < 0)
-            fprintf(stderr, "USB error: %s\n", usb_strerror());
-                else if(nBytes > 0) printf("returned: %i\n", (int)(buffer[0]));
-    }
-        else {
-                int channel = atoi(argv[1]), i;
-                unsigned char* buf = malloc(argc - 2);
-                printf("argc: %i\n", argc);
-                for(i=2; i<argc; ++i) buf[i-2] = atoi(argv[i]);
-                nBytes = usb_control_msg(handle, USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_OUT,
-                                                                        cmd_SetChannelRange, argc-2, channel, buf, argc-2, 5000);
-        fprintf(stderr, "bytes returned: %i\n", nBytes);
-                if(nBytes < 0)
-            fprintf(stderr, "USB error: %s\n", usb_strerror());
-                else if(nBytes > 0) printf("returned: %i\n", (int)(buf[0]));
-                free(buf);
+        } else {
+            if (argv[arg][0]=='0') {
+                values[val++]=0;
+            } else {
+                values[val]=atoi(argv[arg]);
+                if (values[val]==0) {
+                    fprintf(stderr,"Error: Value '%s' not a number or alias\n",argv[arg]);
+                    exit(1);
+                }
+                if ((values[val]<0)||(values[val]>256)) {
+                    fprintf(stderr,"Error: Value '%s' out of range, must be 0-255\n",argv[arg]);
+                    exit(1);
+                }
+                val++;
+            }
         }
+    }
+
+    if (val == 1) {
+        /* Single value = we have a single <channel> / <value> pair */
+        if (verbose) printf("uDMX: Setting channel %d to %d\n", channel, values[val-1]);
+
+        nBytes = usb_control_msg(handle, USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_OUT,
+                                 cmd_SetSingleChannel, values[val-1], channel-1, NULL, 0, 1000);
+        if (nBytes < 0)
+            fprintf(stderr, "USB error: %s\n", usb_strerror());
+    } 
+    if (val > 1) {
+        /* More than 1 value to set, send the array*/
+        if (verbose) {
+            printf("uDMX: Setting channels %d-%d to", channel, channel + val - 1);
+            for (i=0; i<val; i++) printf(" %d",values[i]);
+            printf("\n");
+        }
+
+        nBytes = usb_control_msg(handle, USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_OUT,
+                                 cmd_SetChannelRange, val, channel - 1, (char *)values, val, 1000);
+        if (nBytes < 0)
+            fprintf(stderr, "USB error: %s\n", usb_strerror());
+    }
+    if (val < 1) {
+        fprintf(stderr, "Error: No value found to set\n");
+        exit(1);
+    }
     usb_close(handle);
     return 0;
 }
